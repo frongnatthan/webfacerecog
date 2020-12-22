1bb3f7c9219f63e9418b8005ecccafb55f1e48b4
frongnatthan <natthan.t@ku.th>
2020-12-22 22:12:15 +0700

    setting postgresql

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
-{
+module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": "express-mvp-dbuser",
+    "password": '123456',
+    "database": "express-mvp-db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres",
+    "port": 5432
   },
   "test": {
     "username": "root",
